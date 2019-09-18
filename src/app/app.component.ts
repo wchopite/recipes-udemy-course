282f7616f1014205c79808377a0ae165c3c59c0b
Wladimir Chopite <wchopite@gmail.com>
2019-09-18 14:05:23 -0300

    [first version]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'recipes-angular-udemy';
-}
+export class AppComponent {}
